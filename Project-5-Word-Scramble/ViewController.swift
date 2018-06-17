dc58a58454f88f2c10950506688c1cfb808743b8
David Verbenyi <dverbenyi@gmail.com>
2018-06-17 09:46:19 +0100

    Initial Commit

---
+++ Project-5-Word-Scramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project-5-Word-Scramble
+//
+//  Created by verebes on 12/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+
+}
+
